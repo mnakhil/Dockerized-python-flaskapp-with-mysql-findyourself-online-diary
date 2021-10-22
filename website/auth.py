91c0d45384cbebb7fb7d9ce55daf4bd9bf739424
AKhil Muraleedharan N <anakhil6@gmail.com>
2021-10-22 20:21:56 +0530

    signup.html, about.html completed.

--- website/auth.py
+++ website/auth.py
@@ -7,3 +7,10 @@ auth=Blueprint('auth',__name__)
 def login():
     return render_template("login.html",boolean=True)
 
+@auth.route('/signup')
+def signup():
+    return render_template("signup.html",boolean=True)
+
+@auth.route('/about')
+def about():
+    return render_template("about.html",boolean=True)
